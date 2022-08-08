2b5826c84eec3b9caff96a702e00a3a201a4ab9c
maqiao555 <110798502+maqiao555@users.noreply.github.com>
2022-08-08 16:22:06 +0800

    pull commit

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("push text!!!!!!!!!!!!!!!!!!!");
-
+        System.out.println("pull text!!!!!!!!++++");
     }
 }
